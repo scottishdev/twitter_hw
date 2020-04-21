a8b7c584372fe4efe2fbdb23370f6c76853aa69c
User <user@Users-MacBook-Pro.local>
2020-04-21 20:10:20 +0100

    first commit. got server working

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,51 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template lang="html">
+  <div class="">
+    <ul>
+      <twitter-item v-for="(item, index) in items" :key="index" :item="item"></twitter-item>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TwitterItem from './components/TwitterItem.vue';
 
 export default {
-  name: 'App',
+  name: 'app',
+  data() {
+    return {
+      tweets:[
+        {
+          id: 1,
+          name: 'James',
+          handle: '@jokerjames',
+          img: 'https://semantic-ui.com/images/avatar2/large/matthew.png',
+          tweet: "If you don't succeed, dust yourself off and try again.",
+          likes: 10,
+        },
+        {
+          id: 2,
+          name: 'Fatima',
+          handle: '@fantasticfatima',
+          img: 'https://semantic-ui.com/images/avatar2/large/molly.png',
+          tweet: 'Better late than never but never late is better.',
+          likes: 12,
+        },
+        {
+          id: 3,
+          name: 'Xin',
+          handle: '@xeroxin',
+          img: 'https://semantic-ui.com/images/avatar2/large/elyse.png',
+          tweet: 'Beauty in the struggle, ugliness in the success.',
+          likes: 18,
+        }
+      ]
+    }
+  },
   components: {
-    HelloWorld
+    'twitter-item': TwitterItem
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="css" scoped>
 </style>
---
+++ src/components/TwitterItem.vue
@@ -0,0 +1,26 @@
+<template lang="html">
+  <div class="">
+
+
+  <h1>Twitter</h1>
+  <h3>ID: {{tweet.id}}</h3>
+  <h3>Name: {{tweet.name}}</h3>
+  <h3>Handle: {{tweet.handle}}</h3>
+  <h3>Image: {{tweet.image}}</h3>
+  <h3>Tweet: {{tweet.tweet}}</h3>
+  <h3>Likes: {{tweet.likes}}</h3>
+
+  <h2>Total Likes: {{totalLikes}}</h2>
+  
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'twitter-item',
+  props: ['item']
+}
+</script>
+
+<style lang="css" scoped>
+</style>
